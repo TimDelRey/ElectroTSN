23a06c83e8247aa525ce53b62e287591bff1312c
Тимур Ким <kepaliga@mail.ru>
2025-07-03 15:34:05 +0000

    added profile controller

---
+++ app/controllers/user/profile_controller.rb
@@ -0,0 +1,10 @@
+class User::ProfileController < BaseController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  namespace :user do
+    get "profile/show"
+    get "profile/edit"
+    get "profile/update"
+  end
+  # get "profile/show"
+  # get "profile/edit"
+  # get "profile/update"
   root to: 'welcome#index'
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'custom_devise/registrations' }
 
+  resource :profile, only: [:show, :edit, :update]
 end
