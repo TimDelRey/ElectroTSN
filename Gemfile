ac827149ac8918e92c7512c9ef9b0fd52db6cec6
Тимур Ким <kepaliga@mail.ru>
2025-07-13 13:19:25 +0000

    tests for get download receipt

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem 'aws-sdk-s3', require: false
 gem 'slim-rails'
 gem 'redis'
 gem 'sidekiq'
+gem 'rack'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- spec/controllers/receipts_controller_spec.rb
+++ spec/controllers/receipts_controller_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Receipts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/receipts/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/receipts/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET /download" do
-    it "returns http success" do
-      get "/receipts/download"
-      expect(response).to have_http_status(:success)
+    context 'when receipt has url to s3' do
+      it "redirects to the attached file URL when present" do
+        receipt = create(:receipt)
+        file = fixture_file_upload("files/test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        receipt.xls_file.attach(file)
+
+        get download_receipt_path(receipt)
+
+        expect(response).to redirect_to(rails_blob_url(receipt.xls_file, only_path: true))
+      end
+    end
+
+    context 'when receipt without url to s3' do
+      it "returns 404 if no file is attached" do
+        receipt = create(:receipt)
+
+        get download_receipt_path(receipt)
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'when receipt not found' do
+      it "raises ActiveRecord::RecordNotFound for missing receipt" do
+        expect {
+          get download_receipt_path(id: -1)
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 
