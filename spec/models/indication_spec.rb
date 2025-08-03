a79c445b74655b4f53c914c5c2ded5d0e87f83f0
Тимур Ким <kepaliga@mail.ru>
2025-08-03 20:25:11 +0000

    added duo tests

--- spec/models/indication_spec.rb
+++ spec/models/indication_spec.rb
@@ -23,15 +23,13 @@
 require 'rails_helper'
 
 RSpec.describe Indication, type: :model do
-  # let(:duo_user) { create(:user, tariff: 'duo') }
-
   describe 'testing mono indication' do
     let!(:user) { create(:user, tariff: 'mono') }
     let!(:previous_indication) { create(:indication, for_month: Date.today - 1.month, all_day_reading: 50, user: user) }
 
     subject { build(:indication, user: user) }
 
-    context 'when valid indication' do
+    context 'when valid data' do
       it 'indication now bigger than previous is saved' do
         subject.all_day_reading = 100
 
@@ -83,6 +81,60 @@ RSpec.describe Indication, type: :model do
         expect(subject).not_to be_valid
         expect(subject.save).to eq(false)
         expect(subject.all_day_reading).to be < previous_indication.all_day_reading
+      end
+    end
+  end
+
+  describe 'testing duo indication' do
+    let!(:user) { create(:user, tariff: 'duo') }
+    let!(:previous_indication) { create(:indication, for_month: Date.today - 1.month, day_time_reading: 50, night_time_reading: 40, user: user) }
+
+    subject { build(:indication, user: user) }
+
+    context 'when valid data' do
+      it 'indication now bigger than previous is saved' do
+        subject.day_time_reading = 100
+        subject.night_time_reading = 90
+
+        expect(subject).to be_valid
+        expect(subject.save).to eq(true)
+
+        expect(subject.all_day_reading).to be_nil
+        expect(subject.day_time_reading).to eq(100)
+        expect(subject.night_time_reading).to eq(90)
+        expect(subject.for_month).to eq(Date.today)
+        expect(subject.is_correct).to eq(true)
+        expect(subject.user).to eq(user)
+        expect(subject.day_time_reading).to be > previous_indication.day_time_reading
+        expect(subject.night_time_reading).to be > previous_indication.night_time_reading
+      end
+    end
+
+    context 'when invalid data' do
+      it 'indication is not saved' do
+        subject.day_time_reading = 'beskonechnot ne predel!!!'
+
+        expect(subject).not_to be_valid
+        expect(subject.save).to eq(false)
+      end
+    end
+
+    context 'when indication is different users tariff' do
+      it 'indication is not saved' do
+        subject.all_day_reading = 101
+
+        expect(subject).not_to be_valid
+        expect(subject.save).to eq(false)
+      end
+    end
+
+    context 'when the indication is now less than the previous ones' do
+      it 'indication is not saved' do
+        subject.day_time_reading = 10
+
+        expect(subject).not_to be_valid
+        expect(subject.save).to eq(false)
+        expect(subject.day_time_reading).to be < previous_indication.day_time_reading
       end
     end
   end
