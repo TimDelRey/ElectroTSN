226be3bf4271ff847d2b4bd8df1deaaf53303c92
Тимур Ким <kepaliga@mail.ru>
2025-08-29 09:05:27 +0000

    created tests for create process

--- spec/controllers/moderators/indications_controller_spec.rb
+++ spec/controllers/moderators/indications_controller_spec.rb
@@ -1,98 +1,128 @@
 require 'rails_helper'
 
 RSpec.describe Moderators::IndicationsController, type: :controller do
-  describe 'for users with mono tariff' do
-    subject(:perform_request) { get :show_person, params: request_params }
+  let!(:mono_user) { create(:user, tariff: 'mono') }
+  let!(:duo_user) { create(:user) }
 
-    context 'create process' do
-      context 'when valid data' do
-        it 'indiication mono-tariff is created' do
-        end
-        it 'indication dou-tariff is created' do
-        end
+  before do
+    allow(controller).to receive(:authenticate_user!)
+  end
+
+  describe 'create process' do
+    context 'when valid data' do
+      let(:valid_mono_params) { { user_id: mono_user.id, indication: { all_day_reading: 100 } } }
+      let(:valid_duo_params) { { user_id: duo_user.id, indication: { day_time_reading: 300, night_time_reading: 200 } } }
+
+      it 'indiication mono-tariff is created' do
+        expect { post :create, params: valid_mono_params }.to change(Indication, :count).by(1)
+
+        expect(response).to redirect_to(moderators_indication_path(mono_user))
+        indication = Indication.last
+        expect(indication.user).to eq(mono_user)
+        expect(indication.all_day_reading).to eq(100)
       end
 
-      context 'when reading is empty' do
-        it 'render :new again for mono-traiff' do
-        end
+      it 'indication duo-tariff is created' do
+        expect { post :create, params: valid_duo_params }.to change(Indication, :count).by(1)
 
-        it 'render :new again for duo-traiff' do
-        end
-      end
-    end
-
-    context 'update process' do
-      context 'when valid data' do
-        it 'mono-taiff indiication is updated' do
-        end
-        it 'duo-taiff indiication is updated' do
-        end
-      end
-
-      context 'when reading is empty' do
-        it 'render :edit again for mono-tariff' do
-        end
-        it 'render :edit again for duo-tariff' do
-        end
+        expect(response).to redirect_to(moderators_indication_path(duo_user))
+        indication = Indication.last
+        expect(indication.user).to eq(duo_user)
+        expect(indication.day_time_reading).to eq(300)
+        expect(indication.night_time_reading).to eq(200)
       end
     end
 
-    context 'show action'
-      context 'when click 1 user' do
-        it 'show mono-indications' do
-        end
-        it 'show duo-indications' do
-        end
+    context 'when reading is empty' do
+      let(:invalid_mono_params) { { user_id: mono_user.id, indication: { all_day_reading: '' } } }
+      let(:invalid_duo_params) { { user_id: duo_user.id, indication: { day_time_reading: '', night_time_reading: '' } } }
+      it 'render :new again for mono-traiff' do
+        expect { post :create, params: invalid_mono_params }.not_to change(Indication, :count)
+
+        expect(response).to have_http_status(:unprocessable_entity)
       end
 
-      # context 'when need not default 3 months' do
-      #   it 'show 4 months for mono-tariff' do
-      #   end
-      #   it 'show 2 months for duo-tariff' do
-      #   end
-      # end
+      it 'render :new again for duo-traiff' do
+        expect { post :create, params: invalid_duo_params }.not_to change(Indication, :count)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
     end
+  end
 
-    context 'create collective process' do
-      context 'when valid data' do
-        it 'created indication where readings is present' do
-        end
+  describe 'update process' do
+    context 'when valid data' do
+      it 'mono-taiff indiication is updated' do
       end
-
-      # context 'when need not default 3 months' do
-      #   it 'show 4 months' do
-      #   end
-      # end
-    end
-
-    context 'confirm_month process' do
-      context 'when values of current month are present' do
-        it 'indications are created' do
-        end
-      context 'when current month reading is present' do
-        it 'reading of current month become is_correct: true' do
-        end
-      end
-      context 'when readings of current month are empty' do
-        it 'do nothing' do
-        end
+      it 'duo-taiff indiication is updated' do
       end
     end
 
-    context 'reset metter process' do
-      context 'when indication valid' do
-        it 'created 3 indications' do
-        end
+    context 'when reading is empty' do
+      it 'render :edit again for mono-tariff' do
       end
-
-      context 'when invalid indication' do
-        it 'created new zero indication' do
-        end
-        it 'created same last month indication' do
-        end
-        it 'created same last and new zero indication' do
-        end
+      it 'render :edit again for duo-tariff' do
       end
     end
- end
+  end
+
+  describe 'show action' do
+    context 'when click 1 user' do
+      it 'show mono-indications' do
+      end
+      it 'show duo-indications' do
+      end
+    end
+
+    # context 'when need not default 3 months' do
+    #   it 'show 4 months for mono-tariff' do
+    #   end
+    #   it 'show 2 months for duo-tariff' do
+    #   end
+    # end
+  end
+
+  describe 'create collective process' do
+    context 'when valid data' do
+      it 'created indication where readings is present' do
+      end
+    end
+
+    # context 'when need not default 3 months' do
+    #   it 'show 4 months' do
+    #   end
+    # end
+  end
+
+  describe 'confirm_month process' do
+    context 'when values of current month are present' do
+      it 'indications are created' do
+      end
+    end
+    context 'when current month reading is present' do
+      it 'reading of current month become is_correct: true' do
+      end
+    end
+    context 'when readings of current month are empty' do
+      it 'do nothing' do
+      end
+    end
+  end
+
+  describe 'reset metter process' do
+    context 'when indication valid' do
+      it 'created 3 indications' do
+      end
+    end
+
+    context 'when invalid indication' do
+      it 'created new zero indication' do
+      end
+      it 'created same last month indication' do
+      end
+      it 'created same last and new zero indication' do
+      end
+    end
+  end
 end
