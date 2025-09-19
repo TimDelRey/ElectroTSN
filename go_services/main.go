3e0dddc61afe3fba735c81e361b99ade7fd4b110
Тимур Ким <kepaliga@mail.ru>
2025-09-19 21:44:47 +0000

    config consumer

--- go_services/main.go
+++ go_services/main.go
@@ -5,16 +5,17 @@ import (
     "fmt"
     "time"
 
-    "electrotsn/go_services/pkg/redisqueue"
+    "go_services/pkg/redisqueue"
 )
 
 func main() {
     ctx := context.Background()
 
-    queue := redisqueue.NewQueue("localhost:6379", "", 0, "receipts")
+    // ключ должен совпадать с Sidekiq
+    queue := redisqueue.NewQueue("redis:6379", "", 0, "receipts:jobs")
     consumer := redisqueue.NewConsumer(queue)
 
-    out := make(chan string)
+    out := make(chan redisqueue.ReceiptJob)
 
     go func() {
         if err := consumer.Listen(ctx, out); err != nil {
@@ -25,7 +26,7 @@ func main() {
     for {
         select {
         case job := <-out:
-            fmt.Printf("получил задачу из redis: %s\n", job)
+            fmt.Printf("получил задачу из redis: %+v\n", job)
             return
         case <-time.After(10 * time.Second):
             fmt.Println("ничего не пришло за 10 секунд")
--- go_services/pkg/redisqueue/consumer.go
+++ go_services/pkg/redisqueue/consumer.go
@@ -2,6 +2,8 @@ package redisqueue
 
 import (
     "context"
+    "encoding/json"
+    "fmt"
     "time"
 )
 
@@ -13,18 +15,28 @@ func NewConsumer(q *Queue) *Consumer {
     return &Consumer{Queue: q}
 }
 
-func (c *Consumer) Listen(ctx context.Context, out chan<- string) error {
+func (c *Consumer) Listen(ctx context.Context, out chan<- ReceiptJob) error {
     for {
         select {
         case <-ctx.Done():
             return ctx.Err()
         default:
+            // BLPop ждёт элемент 5 секунд
             res, err := c.Queue.Client.BLPop(ctx, 5*time.Second, c.Queue.Name).Result()
             if err != nil {
+                if err.Error() != "redis: nil" {
+                    fmt.Printf("redis error: %v\n", err)
+                }
                 continue
             }
+
             if len(res) == 2 {
-                out <- res[1]
+                var job ReceiptJob
+                if err := json.Unmarshal([]byte(res[1]), &job); err != nil {
+                    fmt.Printf("json unmarshal error: %v\n", err)
+                    continue
+                }
+                out <- job
             }
         }
     }
